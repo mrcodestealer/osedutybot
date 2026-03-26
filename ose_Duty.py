84ad6387159717baebc17aea01ac91f0bd54c06d
Siah Jun Chen <junchen@MacBook-Air-4.local>
2026-03-26 19:58:10 +0800

    reason

--- ose_Duty.py
+++ ose_Duty.py
@@ -21,7 +21,7 @@ SHEET_ID = "3RIBRL"
 
 # Target names as they appear in column A (case‑insensitive start‑match)
 TARGET_NAMES = [
-    "Louie", "Bryan Peh", "Eduard James", "Chrisjames", "Si Yew",
+    "Louie", "Bryan Peh", "Eduard James", "Chrisjames", "Augustine Siyew",
     "Man Chung", "JanRei", "Katleen", "Lynette", "Chun Chee",
     "Renzel", "Jun Chen", "Kenneth", "Jewel", "Justine Miguel",
     "Kheng Kwan", "Kris Ng"
