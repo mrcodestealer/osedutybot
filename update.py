e8e4f17750ecd78c63542ef8c3243809c3c5f021
Siah Jun Chen <junchen@MacBook-Air-4.local>
2026-04-13 12:45:36 +0800

    update

--- update.py
+++ update.py
@@ -7,9 +7,20 @@ import difflib
 
 # 预定义的有效命令和对应的 (显示名称, 链接)
 UPDATE_MAP = {
-    "fpms prod": ("FPMS PROD SCRIPT", "https://jenkins.client8.me/job/FPMS/job/FPMS_PROD_SCRIPT_RUN/"),
-    "frontend uat1 h5": ("FRONTEND UAT1 H5", "https://jenkins.client8.me/job/FRONTEND/job/UAT/job/projects/job/uat-1/job/h5-uat/"),
-
+    "fpms prod script": ("FPMS PROD SCRIPT", "https://jenkins.client8.me/job/FPMS/job/FPMS_PROD_SCRIPT_RUN/"),
+    "frontend uat1 h5": ("FRONTEND UAT1 H5", "https://jenkins.client8.me/job/FRONTEND/job/UAT/job/projects/job/uat-1/job/h5-uat/build?delay=0sec"),
+    "frontend uat2 h5": ("FRONTEND UAT2 H5", "https://jenkins.client8.me/job/FRONTEND/job/UAT/job/projects/job/uat-2/job/h5-uat/build?delay=0sec"),
+    "frontend uat3 h5": ("FRONTEND UAT3 H5", "https://jenkins.client8.me/job/FRONTEND/job/UAT/job/projects/job/uat-3/job/h5-uat/build?delay=0sec"),
+    "frontend uat4 h5": ("FRONTEND UAT4 H5", "https://jenkins.client8.me/job/FRONTEND/job/UAT/job/projects/job/uat-4/job/h5-uat/build?delay=0sec"),
+    "frontend uat1 web": ("FRONTEND UAT1 WEB", "https://jenkins.client8.me/job/FRONTEND/job/UAT/job/projects/job/uat-1/job/web-uat/build?delay=0sec"),
+    "frontend uat2 web": ("FRONTEND UAT2 WEB", "https://jenkins.client8.me/job/FRONTEND/job/UAT/job/projects/job/uat-2/job/web-uat/build?delay=0sec"),
+    "frontend uat3 web": ("FRONTEND UAT3 WEB", "https://jenkins.client8.me/job/FRONTEND/job/UAT/job/projects/job/uat-3/job/web-uat/build?delay=0sec"),
+    "frontend uat4 web": ("FRONTEND UAT4 WEB", "https://jenkins.client8.me/job/FRONTEND/job/UAT/job/projects/job/uat-4/job/web-uat/build?delay=0sec"),
+    "fpms uat fgs": ("FPMS FGS", "https://jenkins.client8.me/job/FGS_CLIENT/job/FGS-UAT-UPDATE/build?delay=0sec"),
+    "ccms uat fe bo": ("FPMS_NT_UAT_BO_UPDATE", "https://jenkins.client8.me/job/FPMS_NT/view/all/job/FPMS_NT_UAT_BO_UPDATE/build?delay=0sec"),
+    "fpms uat fnt":("FPMS FNT","https://jenkins.client8.me/job/FNT/job/FNT_UAT_SCRIPT_RUN/build?delay=0sec"),
+    "cpms uat update":("CPMS-UAT-UPDATE","\nhttps://jenkins.client8.me/job/FNT/job/FNT_UAT_SCRIPT_RUN/build?delay=0sec\nhttps://jenkins.client8.me/job/IGO/job/UAT/job/IGO-UAT-UPDATE/build?delay=0sec"),
+    "igo uat script run":("IGO UAT SCRIPT RUN","https://jenkins.client8.me/job/IGO/job/UAT/job/IGO-UAT-SCRIPT-RUN/")
 }
 
 # 所有有效命令的列表（用于模糊匹配）
