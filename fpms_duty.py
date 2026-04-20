af133e91f30ea5c92d270671b7f5e22340d4a61c
Siah Jun Chen <junchen@MacBook-Air-4.local>
2026-04-20 09:34:32 +0800

    fpms

--- fpms_duty.py
+++ fpms_duty.py
@@ -370,7 +370,7 @@ def fpmsp0():
         output_lines.append("")   # 空行分隔
 
     # 添加固定联系人列表
-    output_lines.append("<b>On-duty → BK → Yang → Eason</b>")
+    output_lines.append("<b>FYI On-duty → BK → Yang → Eason</b>\n")
     output_lines.append("<b>Addtional Contact number for P0</b>")
     output_lines.append("• BK 📞 601133798396")
     output_lines.append("• Yang 📞 60168109045")
